e348c47457b3761138a4bc00a26e41a87d3c48b3
Divyansh Agrawal <divyansh1001agrawal@gmail.com>
2025-09-20 15:44:00 +0530

    Project Completed

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cors({
 	origin: process.env.CLIENT_URL || "http://localhost:5173",
 	credentials: true,
 }));
-app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
+app.use(express.json({ limit: "10mb" })); 
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
